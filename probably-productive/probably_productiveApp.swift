20ae4dcbcbc806bf141630817d65548d0685dab8
MontyNau <85969863+montynau@users.noreply.github.com>
2026-03-06 19:01:51 +0200

    Initial Commit

---
+++ probably-productive/probably_productiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  probably_productiveApp.swift
+//  probably-productive
+//
+//  Created by Monty on 06/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct probably_productiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
